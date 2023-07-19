8bee4d7ba600282617282848f9798aa20908ea23
GANGADASARISAILAKSHMI <130964719+GANGADASARISAILAKSHMI@users.noreply.github.com>
2023-07-19 18:59:48 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World");
+		//System.out.println("Hello world");
+		//int a=10;
+		//System.out.println(a);
+		//System.out.print("Value of a is :" + a);
+		//int x;
+		//First t=new First();
+		//Scanner sc=new Scanner(System.in);
+		//System.outss.println("Enter the value of x: ");
+		//x=sc.nextInt();
+		//System.out.println("Value of x is : "+ x);
+		//int a,b;
+		//System.out.println("Enter the value of a: ");
+		//a=sc.nextInt();
+		//System.out.println("Enter the value of b: ");
+		//b=sc.nextInt();
+		//System.out.println("The sum is :"+(a+b));
+		int a=7;
+		int b=++a;
+		int c=a++;
+		System.out.println(a+ b+ c);
+
+	}
+
+}
