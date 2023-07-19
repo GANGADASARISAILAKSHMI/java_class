305c149b0c67b32677a23a7253a822ad4339f03b
GANGADASARISAILAKSHMI <130964719+GANGADASARISAILAKSHMI@users.noreply.github.com>
2023-07-19 19:01:26 +0530

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,22 @@
+
+public class Teacher {
+	String name;
+	private int id;
+	
+public int getId() {
+		return id;
+	}
+public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+
+public void println()
+{
+	System.out.println("The id is:"+id+"The string is:"+name);
+}
+}
+
