6a9504f3b029c2452fa648c4c7ce58d09745623c
GANGADASARISAILAKSHMI <130964719+GANGADASARISAILAKSHMI@users.noreply.github.com>
2023-07-19 19:03:03 +0530

    Create TeacherUse.java

---
+++ TeacherUse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TeacherUSe {
+	public static void main(String args[]) {
+		
+		//Teacher t2=new Teacher();
+		//t2.id=102;
+		//t2.name="Lucky";
+		//t2.print();
+		//System.out.println("Enter the id and name:");
+		//Scanner sc=new Scanner(System.in);
+		//int n=sc.nextInt();
+		//String s=sc.next();
+		Teacher t2=new Teacher();
+		//t2.name=s;
+		//t2.id=n;
+		t2.setId(101);
+		t2.name="Sai";
+		System.out.println("The name is : " +t2.name);
+		System.out.print("The is is : " +t2.getId());
+		
+		
+		
+		}
+	}
+	
+
